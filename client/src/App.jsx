69c90e5e7a9fa2dbe2a728137785c68540670a3f
Matthew Bailey <matthewbailey.webdev@gmail.com>
2024-07-12 14:41:22 -0600

    got react and tailwind working and started navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 // client/src/App.jsx
 
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,27 +17,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Navbar/>
+     
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        
+       
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-          <h1>{!data ? "Loading..." : data}</h1>
+          <h1 className="bg-red-500">{!data ? "Loading..." : data}</h1>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
